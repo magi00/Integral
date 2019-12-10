3926ebc05af849a0b811da8feb6ba7a3d96743df
uibukant <57043902+uibukant@users.noreply.github.com>
2019-12-10 15:19:36 +0200

    Update project.py

--- project.py
+++ project.py
@@ -25,11 +25,11 @@ def test():
         funktsioonid = [((randint(1,100)*x+randint(1,100))**randint(2,10)),(randint(1,100)/(randint(1,100)*x)),((cos(x)**randint(1,3))*(sin(x)**randint(1,3))), (log(pi+randint(1,10))*cos(x)**randint(0,3)),]
         i =randint(0,1)
         funktsioon = funktsioonid[i]
-        esimene_ülesanne = print("Sinu ülesanne: integreeri funktsiooni ", '\n')
-        esimene_ülesanne = pprint(funktsioon, '\n')
+        print("Sinu ülesanne: integreeri funktsiooni ", '\n')
+        pprint(funktsioon, '\n')
         mbox.showerror("Vihje","Selleks, et automaatkontroll loeks sinu vastust õigeks, pead sisestama enda vastust näiteks sellisel kujul: 35*log(x)/47", parent=application_window)
         tulemus = del_tühikud(str(integrate(funktsioon,x,manual=True)))
-        vastus = simpledialog.askstring("Sisesta","Sisesta vastust: ",parent=application_window)
+        vastus = simpledialog.askstring("Sisesta","Sinu ülesanne on kuvatud ekraanil. Sisesta vastust: ",parent=application_window)
         if vastus == tulemus:
             otsus = simpledialog.askstring("Sisesta","Sinu vastus oli õige! Sisesta 'jah' kui soovid jätkata ning 'ei' kui soovid väljuda: ",parent=application_window)
             if otsus == 'ei':
@@ -71,29 +71,48 @@ def test():
         print('Sinu ülesanne: leia funktsiooni tuletis')
         pprint(funktsioonid[i])
         mbox.showerror("Vihje","Selleks, et automaatkontroll loeks sinu vastust õigeks, pead sisestama enda vastust näiteks sellisel kujul: 35*log(x)/47", parent=application_window)
-        vastus=del_tühikud(simpledialog.askstring("Sisesta",'Sisesta vastust: ',parent=application_window))
+        vastus=del_tühikud(simpledialog.askstring("Sisesta",'Sinu ülesanne on kuvatud ekraanil. Sisesta vastust: ',parent=application_window))
         tulemus=del_tühikud(str(diff(funktsioonid[i])))
-        if vastus!= str(tulemus):
-            while vastus!=str(tulemus):
-                mbox.showerror("Vale vastus!","Vale vastus! Selleks, et automaatkontroll loeks sinu vastust õigeks, pead sisestama enda vastust näiteks sellisel kujul: 35*log(x)/47 ", parent=application_window)
-                vastus = simpledialog.askstring("Sisesta","Kui soovid näha õiget vastust siis sisesta 'näita'. Kui soovid jätkata siis proovi veel korra! Sisesta vastus uuesti : ",parent=application_window)
-                if vastus=='näita':
-                    print("Õige vastus on: ")
-                    pprint(diff(funktsioonid[i]))
-                    print("või: ", '\n',diff(funktsioonid[i]), '\n')
+        if vastus == tulemus:
+            otsus = simpledialog.askstring("Sisesta","Sinu vastus oli õige! Sisesta 'jah' kui soovid jätkata ning 'ei' kui soovid väljuda: ",parent=application_window)
+            if otsus == 'ei':
+                mbox.showerror("Viimane sõnum", "Oli meeldiv sinuga töötada, edu õppimises! ", parent=application_window)
+                return False
+            else:
+                teema = simpledialog.askstring("Sisesta", "Sina saad valida kaks teemat: kas integraal või tuletis. Sisesta 'integraal' selleks, et lahendada integraali ülesandeid, või siis 'tuletis' selleks, et lahendada tuletis ülesandeid. Sisesta teema: ",parent=application_window)
+        while vastus != str(tulemus):
+            mbox.showerror("Vale vastus!","Vale vastus! Selleks, et automaatkontroll loeks sinu vastust õigeks, pead sisestama enda vastust näiteks sellisel kujul: 35*log(x)/47 ", parent=application_window)
+            vastus = del_tühikud(simpledialog.askstring("Sisesta", "Kui soovid näha õiget vastust siis sisesta 'näita'. Kui soovid jätkata siis proovi veel korra! Selleks, et automaatkontroll loeks sinu vastust õigeks, pead sisestama enda vastust näiteks sellisel kujul: 35*log(x)/47 või sin(x)**4/4. Sisesta vastus uuesti : ",parent=application_window))
+            if vastus == str(tulemus):
+                otsus = simpledialog.askstring("Sisesta", "Sinu vastus oli õige! Sisesta 'jah' kui soovid jätkata ning 'ei' kui soovid väljuda: ",parent=application_window)
+                if otsus == 'ei':
+                    mbox.showerror("Viimane sõnum", "Oli meeldiv sinuga töötada, edu õppimises! ", parent=application_window)
+                    return False
+            if vastus == 'näita':
+                print('\n', "Õige vastus on: ", '\n')
+                pprint(del_tühikud(str(diff(funktsioonid[i]))))
+                print( '\n', "või: ", '\n', '\n',del_tühikud(str(diff(funktsioonid[i]))), '\n')
+                otsus = simpledialog.askstring("Sisesta", "Õige vastus on kuvatud ekraanil. Kas jätkame? Sisesta 'jah' kui soovid jätkata või sisesta 'ei' kui soovid väljuda: ",parent=application_window)
+                if otsus == 'ei':
+                    mbox.showerror("Viimane sõnum", "Oli meeldiv sinuga töötada, edu õppimises! ", parent=application_window)
+                    return False
+                elif otsus == 'jah':
+                    teema = simpledialog.askstring("Sisesta", "Sina saad valida kaks teemat: kas integraal või tuletis. Sisesta 'integraal' selleks, et lahendada integraali ülesandeid, või siis 'tuletis' selleks, et lahendada tuletis ülesandeid. Sisesta teema: ",parent=application_window)
                     break
-            otsus = simpledialog.askstring("Sisesta","Kas jätkame? Sisesta 'jah' kui soovid jätkata või sisesta 'ei' kui soovid väljuda: ",parent=application_window).strip()
-        if vastus==str(tulemus):
-            otsus = simpledialog.askstring("Sisesta","Sinu vastus oli õige! Kas jätkame? Sisesta 'jah' kui soovid jätkata või sisesta 'ei' kui soovid väljuda: ",parent=application_window).strip()
-        if otsus == 'ei':
-            print("Oli meeldiv sinuga töötada, edu õppimises! ")
-            break
-        else:
-            teema = simpledialog.askstring("Sisesta","Sina saad valida kaks teemat: kas integraal või tuletis. Sisesta 'integraal' selleks, et lahendada integraali ülesandeid, või siis 'tuletis' selleks, et lahendada tuletis ülesandeid. Sisesta teema: ",parent=application_window).strip()
-        i+=1
+                else:
+                    while True:
+                        mbox.showerror("Error","Ootasin vastust kas 'jah' või 'ei'.", parent=application_window)
+                        otsus = simpledialog.askstring("Sisesta", "Kas jätkame? Sisesta 'jah' kui soovid jätkata või sisesta 'ei' kui soovid väljuda: ",parent=application_window)
+                        if otsus == 'jah':
+                            return test()
+                        else:
+                            mbox.showerror("Viimane sõnum", "Oli meeldiv sinuga töötada, edu õppimises! ", parent=application_window)
+                            return False
+            
+            
+   
 
 
 test()
-
 
 
